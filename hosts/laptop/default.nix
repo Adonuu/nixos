65f39ad025ac1147914188424b6ab3334b11d3a4
Adam Young <adamyoung1414@gmail.com>
2026-07-18 18:18:11 -0500

    add nvidia drivers and hybrid setup for gtx1080

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -38,5 +38,29 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics.enable = true;
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false; # Set to false to avoid sleep issues
+    powerManagement.finegrained = false; # Must be false (GTX 1060 does not support this)
+    open = false; # Must be false (Nvidia open drivers require Turing or newer)
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580; #1080 is only supported up to 580 driver
+
+    nvidiaSettings = true;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
   system.stateVersion = "26.05";
 }
