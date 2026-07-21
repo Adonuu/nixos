e79bb405e407a72c74b674b47e4243db1c984e77
Adam Young <adamyoung1414@gmail.com>
2026-07-21 18:24:47 -0500

    update kitty to use symlink

--- modules/apps/kitty.nix
+++ modules/apps/kitty.nix
@@ -1,17 +1,11 @@
 { config, pkgs, ... }: {
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "JetBrainsMono Nerd Font";
-      size = 11;
-    };
-    settings = {
-      scrollback_lines = 10000;
-      background_opacity = "0.95";
-      window_padding_width = 12;
-      enable_audio_bell = "no";
-      update_check_interval = 0; # NixOS handles updates
-      confirm_os_window_close = 0;
-    };
+
+  xdg.configFile."kitty" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/config/kitty";
+    recursive = true;
   };
+
+  home.packages = [
+    pkgs.kitty
+  ];
 }
